674be66116c3ab2aa75a638dc02e5352b098c69a
Isaac Mattern <isaac.mattern@gmail.com>
2025-12-05 10:57:59 -0500

    Complete day 5 part 2

--- Day_5/part2.py
+++ Day_5/part2.py
@@ -10,18 +10,44 @@ def getInput():
   input = open(filename, 'r')
   return input
 
+def combineRanges(ranges: list[list[int]]) -> list[list[int]]:
+  sortedRanges = sorted(ranges)
+  
+  combinedRanges = [sortedRanges[0]]
+  sortedRanges = sortedRanges[1:]
+  
+  for range in sortedRanges:
+    if range[0] <= combinedRanges[-1][1]:
+      combinedRanges[-1][1] = max(combinedRanges[-1][1], range[1])
+    else:
+      combinedRanges.append(range)
+  
+  return combinedRanges
+
 def solve():
   
   input = getInput()
   
-  print("Beginning of Input\n")
-  
+  ranges = []
+  ids = []
+  idsStarted = False
   for line in input:
-    print(line)
-    
-  print("\nEnd of Input")
+    if not idsStarted:
+      if line[0] == '\n':
+        idsStarted = True
+      else:
+        trimmedRange = line.strip()
+        beginningStr, endStr = trimmedRange.split('-')
+        ranges.append([int(beginningStr), int(endStr)])
+    else:
+      ids.append(int(line.strip()))
+      
+  combinedRanges = combineRanges(ranges)
+  ans = 0 
   
-  ans = 0
+  for range in combinedRanges:
+    ans += range[1] - range[0] + 1
+    
   print(ans)      
 
 solve()
