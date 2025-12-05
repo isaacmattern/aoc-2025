9c09f05eed459436c032f12e7d6ae6887b5da976
Isaac Mattern <isaac.mattern@gmail.com>
2025-12-05 10:56:20 -0500

    Complete day 5 part 1

--- Day_5/part1.py
+++ Day_5/part1.py
@@ -10,18 +10,56 @@ def getInput():
   input = open(filename, 'r')
   return input
 
+def combineRanges(ranges: list[list[int]]) -> list[list[int]]:
+  sortedRanges = sorted(ranges)
+  
+  combinedRanges = [sortedRanges[0]]
+  sortedRanges = sortedRanges[1:]
+  
+  for range in sortedRanges:
+    if range[0] <= combinedRanges[-1][1]:
+      combinedRanges[-1][1] = max(combinedRanges[-1][1], range[1])
+    else:
+      combinedRanges.append(range)
+  
+  return combinedRanges
+
 def solve():
   
   input = getInput()
   
-  print("Beginning of Input\n")
-  
+  ranges = []
+  ids = []
+  idsStarted = False
   for line in input:
-    print(line)
-    
-  print("\nEnd of Input")
+    if not idsStarted:
+      if line[0] == '\n':
+        idsStarted = True
+      else:
+        trimmedRange = line.strip()
+        beginningStr, endStr = trimmedRange.split('-')
+        ranges.append([int(beginningStr), int(endStr)])
+    else:
+      ids.append(int(line.strip()))
+      
+  combinedRanges = combineRanges(ranges)
+  ans = 0 
   
-  ans = 0
+  for id in ids:
+    # Run binary search to find a valid range
+    low = 0
+    high = len(combinedRanges) - 1
+    
+    while low <= high:
+      mid = (low + high) // 2
+      if id < combinedRanges[mid][0]:
+        high = mid - 1
+      elif id > combinedRanges[mid][1]:
+        low = mid + 1
+      else:
+        ans += 1
+        break
+    
   print(ans)      
 
 solve()
