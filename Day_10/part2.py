19355e482053c4c5a36b02070a379d449133db2e
Isaac Mattern <isaac.mattern@gmail.com>
2025-12-10 16:42:24 -0500

    Set up day 10 part 2

--- Day_10/part2.py
+++ Day_10/part2.py
@@ -1,7 +1,25 @@
 import sys
+import math
 
+class Machine:
+  def __init__(self, targetState: tuple[bool], buttons: tuple[tuple[int]], joltages: tuple[int]):
+    if not isinstance(targetState, tuple) or not all(isinstance(x, bool) for x in targetState):
+      raise ValueError("targetState must be a tuple of bools")
+
+    if not isinstance(buttons, tuple) or not all(
+        isinstance(b, tuple) and all(isinstance(x, int) for x in b)
+        for b in buttons
+    ):
+      raise ValueError("buttons must be a tuple of tuples of ints")
+
+    if not isinstance(joltages, tuple) or not all(isinstance(x, int) for x in joltages):
+      raise ValueError("joltages must be a tuple of ints")
+
+    self.targetState = targetState
+    self.buttons = buttons
+    self.joltages = joltages
+    
 def getInput():
-  
   if len(sys.argv) < 2:
     print("Usage: python3 part2.py <filename>")
     quit()
@@ -10,18 +28,59 @@ def getInput():
   input = open(filename, 'r')
   return input
 
-def solve():
-  
-  input = getInput()
-  
-  print("Beginning of Input\n")
-  
+def parseInput(input) -> list[Machine]:
+  machines = []
   for line in input:
-    print(line)
+    trimmedLine = line.strip()
+    openParenIndex = trimmedLine.index("[")
+    closeParenIndex = trimmedLine.index("]")
+    openCurlyIndex = trimmedLine.index("{")
+    closeCurlyIndex = trimmedLine.index("}")
     
-  print("\nEnd of Input")
+    indicatorLightsTargetString = trimmedLine[openParenIndex+1:closeParenIndex]
+    targetState = tuple([True if char == "#" else False for char in indicatorLightsTargetString])
+    
+    buttons = []
+    buttonSchematicStrings = trimmedLine[closeParenIndex+2:openCurlyIndex-1]
+    for buttonSchematic in buttonSchematicStrings.split(" "):
+      numbersWithCommas = buttonSchematic[1:len(buttonSchematic)-1]
+      button = tuple([int(num) for num in numbersWithCommas.split(",")])
+      buttons.append(button)
+    buttons = tuple(buttons)
+      
+    joltagesString = trimmedLine[openCurlyIndex+1:closeCurlyIndex]
+    joltages = tuple([int(num) for num in joltagesString.split(",")])
+    
+    newMachine = Machine(targetState=targetState, buttons=buttons, joltages=joltages)
+    machines.append(newMachine)
+    
+  return machines
   
-  ans = 0
-  print(ans)      
+
+def solve():
+  input = getInput()
+  machines = parseInput(input)
+  
+  def dfs(machine: Machine, curr: list[bool], i: int, buttonsPushed: int) -> int:
+    if i == len(machine.buttons):
+      if machine.targetState == tuple(curr):
+        return buttonsPushed
+      else:
+        return math.inf
+    
+    updatedButtons = curr.copy()
+    for buttonIndex in machine.buttons[i]:
+      updatedButtons[buttonIndex] = not updatedButtons[buttonIndex]
+    pushButton = dfs(machine, updatedButtons, i + 1, buttonsPushed + 1)
+      
+    doNotPushButton = dfs(machine, curr, i + 1, buttonsPushed)
+    return min(pushButton, doNotPushButton)
+  
+  minButtonPushesPerMachine = []
+  for machine in machines:
+    start = [False] * len(machine.targetState)
+    minButtonPushesPerMachine.append(dfs(machine, start, 0, 0))
+    
+  print(sum(minButtonPushesPerMachine))      
 
 solve()
