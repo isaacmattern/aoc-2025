fb5e5a574782f7a413632c85751f09cc046795a6
Isaac Mattern <isaac.mattern@gmail.com>
2025-12-10 16:01:42 -0500

    Parse input for day 10

--- Day_10/part1.py
+++ Day_10/part1.py
@@ -1,7 +1,6 @@
 import sys
 
 def getInput():
-  
   if len(sys.argv) < 2:
     print("Usage: python3 part1.py <filename>")
     quit()
@@ -13,14 +12,31 @@ def getInput():
 def solve():
   
   input = getInput()
-  
-  print("Beginning of Input\n")
-  
-  for line in input:
-    print(line)
     
-  print("\nEnd of Input")
-  
+  machines = []
+  for line in input:
+    trimmedLine = line.strip()
+    openParenIndex = trimmedLine.index("[")
+    closeParenIndex = trimmedLine.index("]")
+    openCurlyIndex = trimmedLine.index("{")
+    closeCurlyIndex = trimmedLine.index("}")
+    
+    indicatorLightsTargetString = trimmedLine[openParenIndex+1:closeParenIndex]
+    targetState = tuple([True if char == "#" else False for char in indicatorLightsTargetString])
+    
+    buttons = []
+    buttonSchematicStrings = trimmedLine[closeParenIndex+2:openCurlyIndex-1]
+    for buttonSchematic in buttonSchematicStrings.split(" "):
+      numbersWithCommas = buttonSchematic[1:len(buttonSchematic)-1]
+      button = tuple([int(num) for num in numbersWithCommas.split(",")])
+      buttons.append(button)
+    buttons = tuple(buttons)
+      
+    joltagesString = trimmedLine[openCurlyIndex+1:closeCurlyIndex]
+    joltages = tuple([int(num) for num in joltagesString.split(",")])
+    
+    machines.append((targetState, buttons, joltages))
+      
   ans = 0
   print(ans)      
 
